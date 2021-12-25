a0d2a8360a3b3f06b3a995f82fe5d5055c6b7ad3
badjr <badjr1@gmail.com>
2021-12-25 01:01:12 -0500

    Adding source for Dao interface.

--- src/main/java/com/badjr/studyboost/dao/Dao.java
+++ src/main/java/com/badjr/studyboost/dao/Dao.java
@@ -3,6 +3,9 @@ package com.badjr.studyboost.dao;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * From https://baeldung.com/java-dao-pattern
+ */
 public interface Dao<T> {
 	Optional<T> get(long id);
 
